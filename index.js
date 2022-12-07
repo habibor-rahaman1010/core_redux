4dc8fbe3f98168f201f9bee22bff2232eeff3c94
habibor-rahaman1010 <habibor144369@gmail.com>
2022-12-07 21:31:46 +0600

    ok

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+//constant type...
+const INCREMENT = 'INCREMENT';
+const ADD_PERSION = 'ADD_PERSION';
+
 //initial state...
+const counterState = {
+    count: 0,
+    person:[
+        {id: 1, name: 'habibor', address: 'dhaka'}
+    ]
+} 
+
