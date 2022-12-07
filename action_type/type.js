d2c37beabc864b6984c9cb195fd6dd5621a5df92
habibor-rahaman1010 <habibor144369@gmail.com>
2022-12-07 23:28:20 +0600

    ok

--- action_type/type.js
+++ action_type/type.js
@@ -1,6 +1,6 @@
 //action type...
-const INCREMENT = 'INCREMENT';
-const DECREMENT = "DECREMENT";
-const RESET = 'RESET'
-const ADD_PERSION = 'ADD_USER';
-const COUNT_USER = 'COUNT_USER'
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = "DECREMENT";
+export const RESET = 'RESET'
+export const ADD_PERSION = 'ADD_USER';
+export const COUNT_USER = 'COUNT_USER'
