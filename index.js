a5b4052d025acdee2438b110f0aaaadcd021c457
habibor-rahaman1010 <habibor144369@gmail.com>
2022-12-07 23:23:48 +0600

    ok

--- index.js
+++ index.js
@@ -96,9 +96,11 @@ store.subscribe(() => {
 //dispatch action...
 
 store.dispatch(increment());
-
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
 store.dispatch(reset());
 store.dispatch(addUser({id: 2, name: 'Aayan', address: 'mohammodpur'}));
-
+store.dispatch(addUser({id: 3, name: 'Tamim', address: 'Dhanmondi'}));
 store.dispatch(countUser());
 
