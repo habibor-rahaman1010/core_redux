49bf8501985fde79e62b8ae3ff30d0ce4ef6df0a
habibor-rahaman1010 <habibor144369@gmail.com>
2022-12-07 23:11:42 +0600

    ok

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const INCREMENT = 'INCREMENT';
 const DECREMENT = "DECREMENT";
 const RESET = 'RESET'
 const ADD_PERSION = 'ADD_USER';
+const COUNT_USER = 'COUNT_USER'
 
 //initial state...
 const initialState = {
     count: 0,
     user:[
         {id: 1, name: 'habibor', address: 'dhaka'}
-    ]
+    ],
+    totalUser: 1,
 } 
 
 const increment = () => {
@@ -39,31 +41,43 @@ const addUser = (user) => {
     }
 }
 
+const countUser = () =>{
+    return {
+        type: COUNT_USER,
+    }
+}
+
 //it's my reducer function 
 const counterReducer = (state = initialState, action) => {
     switch (action.type) {
         case INCREMENT:
-            return{
+            return {
                 ...state,
                 count: state.count + 1,
             }
 
         case DECREMENT:
-            return{
+            return {
                 ...state,
                 count: state.count - 1,
             }
 
         case RESET:
-            return{
+            return {
                 ...state,
                 count: 0,
             }
         
         case ADD_PERSION:
-            return{
+            return {
                 ...state,
                 person: [...state.user, action.payload],
+            }
+
+        case COUNT_USER:
+            return {
+                ...state,
+                totalUser: state.user.length;
             }
 
         default:
