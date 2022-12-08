74dd340ba95fe791c702ff332064655be321a1e4
habibor-rahaman1010 <habibor144369@gmail.com>
2022-12-08 18:04:50 +0600

    hello programmer

---
+++ redux/redux.js
@@ -0,0 +1,14 @@
+// initial state of project...
+const initialSatate = {
+    product:[{
+        id: 1,
+        name: "Hp eleitbook Laptop",
+        brand: "Hp",
+        quantity: 1,
+        price: 56000,
+        reting: 4.5,
+    }],
+    totalProduct: 1,
+    availableProduct: 0,
+    bestSeller: 0,
+}
