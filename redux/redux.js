3401021e14777c5d4a103e87e188263aef15d62c
habibor-rahaman1010 <habibor144369@gmail.com>
2022-12-08 18:11:07 +0600

    hello programmer

--- redux/redux.js
+++ redux/redux.js
@@ -1,3 +1,11 @@
+//constant variable my project...
+const GET_PRODUCT = "GET_PRODUCT";
+const ADD_PRODUCT = "ADD_PRODUCT";
+const TOTAL_PRODUCT = "TOTAL_PRODUCT";
+const AVAILABLE_PRODUCT = "AVAILABLE_PRODUCT";
+const BEST_SELLER = "BEST_SELLER";
+
+
 // initial state of project...
 const initialSatate = {
     product:[{
@@ -7,6 +15,7 @@ const initialSatate = {
         quantity: 1,
         price: 56000,
         reting: 4.5,
+        status: true,
     }],
     totalProduct: 1,
     availableProduct: 0,
